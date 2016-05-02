e5551f84617b5bb5385a2a52694b35654c945392
stevebasford <mail@stevebasford.com>
2016-05-02 23:49:44 +0100

    before 1st test

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,57 @@
 var gulp = require('gulp');
+// css
+var postcss      = require('gulp-postcss');
+var precss       = require('precss');
+var autoprefixer = require('autoprefixer')
+var cssnano      = require('cssnano');
+// Images
+var imagemin     = require('gulp-imagemin');
+// Clean
+var del          = require('del');
+
+
+
+// - - - Paths - - - //
+var paths = {
+  css: 'src/css/**/*.css', // Will need a rule hear for ignoring partials
+  images: 'src/img/**/*'
+};
+
+
+// - - - Tasks - - - //
+
+// cli
+gulp.task('build', gulp.series(
+  clean,
+  gulp.parallel(css, images)
+));
+
+// Default
+gulp.task('default', gulp.series('build'));
+
+
+// - - - Functions - - - //
+
+// css
+var processors = [
+  precss,
+  autoprefixer({browsers: ['last 2 versions']}),
+  css nano,
+];
+function css() {
+  return gulp.src(paths.css)
+    .pipe(postcss(processors))
+    .pipe(gulp.dest('build/css'));
+});
+
+// Images
+function images() {
+  return gulp.src(paths.images)
+    .pipe(imagemin({optimizationLevel: 5}))
+    .pipe(gulp.dest('build/img'))
+};
+
+// Clean
+function clean(done) {
+  del(['build'], done)
+};
